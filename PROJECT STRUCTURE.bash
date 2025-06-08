92404fc05065036f0c4389f41255235a7f68ab7f
rahul chirra <94059294+rahulchirra@users.noreply.github.com>
2025-06-08 09:38:17 +0530

    Delete PROJECT STRUCTURE.bash

--- PROJECT STRUCTURE.bash
+++
@@ -1,6 +0,0 @@
-├── main.py                # Main bot + GUI file
-├── drivers/
-│   └── chromedriver.exe   # Must match browser version
-├── credentials.json       # Auto-created if "Remember Me" is checked
-├── balance_graph.png      # Auto-generated graph after stop
-└── README.md              # You're reading it!
